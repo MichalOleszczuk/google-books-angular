c93e30f325143abb4537df2d1096eb6df6c8b13a
Michał Oleszczuk <oleszczuk.m@gmail.com>
2018-11-08 20:13:15 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChildComponent } from './content-wrapper/child.component';
+import { HeaderComponent } from './common/navigation/header/header.component';
+import { FooterComponent } from './common/navigation/footer/footer.component';
+import { ContentWrapperComponent } from './content-wrapper/content-wrapper.component';
+
+// Add an icon to the library for convenient access in other components
+library.add(fas);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContentWrapperComponent,
+    ChildComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
